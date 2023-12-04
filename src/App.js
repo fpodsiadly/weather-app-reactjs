e39216079130a2fe7941a1055a47d6f9d3b983dd
Filip <fpodsiadly@gmail.com>
2023-12-04 23:08:41 +0100

    top bar and input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopBar from './Components/TopBar'
+import Inputs from './Components/Inputs'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopBar />
+      <Inputs />
     </div>
   )
 }
