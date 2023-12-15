7acbb418637f49ed6b49605d9a87673c14484b6f
Filip <fpodsiadly@gmail.com>
2023-12-15 22:29:04 +0100

    populate data from api

--- src/App.js
+++ src/App.js
@@ -5,17 +5,36 @@ import Inputs from './Components/Inputs'
 import TimeAndLocation from './Components/TimeAndLocation'
 import TempAndDetails from './Components/TempAndDetails'
 import Forecast from './Components/Forecast'
-
+import getFormattedWeatherData from './Services/weatherService'
+import { useEffect, useState } from 'react'
 function App() {
+  const [query, setQuery] = useState({ q: 'lodz' })
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data)
+      })
+    }
+    fetchWeather()
+  }, [query, units])
+
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className="mx-auto max-w-screen-md mt-4 pt-5 pb-10 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopBar />
       <Inputs />
 
-      <TimeAndLocation />
-      <TempAndDetails />
-      <Forecast title="hourly forecast" />
-      <Forecast title="daily forecast" />
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetails weather={weather} />
+
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </>
+      )}
     </div>
   )
 }
